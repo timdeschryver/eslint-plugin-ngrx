2eed1a4dbfb1952d45400085f7046af5d909d643
Julien Saguet <julien.saguet@outlook.fr>
2020-05-25 00:16:29 +0200

    feat: adds no-multiple-stores rule

--- src/rules/utils/selectors/index.ts
+++ src/rules/utils/selectors/index.ts
@@ -1,8 +1,17 @@
 export const effectCreator = `ClassProperty[value.callee.name='createEffect']`
+
 export const effectDecorator = `ClassProperty > Decorator[expression.callee.name='Effect']`
+
 export const actionCreator = `CallExpression[callee.name=/createAction.*/]`
+
 export const reducerOn = `CallExpression[callee.name='createReducer'] > CallExpression[callee.name='on']`
-export const store = `MethodDefinition[kind='constructor'] TSTypeReference[typeName.name='Store'][typeParameters.params]`
+
+export const constructorExit = `MethodDefinition[kind='constructor']:exit`
+
 const actionDispatch =
   'ExpressionStatement:has(CallExpression > MemberExpression:has(Identifier[name="dispatch"]):has(MemberExpression > Identifier[name="store"]))'
 export const multipleActionDispatch = `${actionDispatch} ~ ${actionDispatch}`
+
+export const injectedStore = `MethodDefinition[kind='constructor'] Identifier>TSTypeAnnotation>TSTypeReference[typeName.name="Store"]`
+
+export const typedStore = `${injectedStore}[typeParameters.params]`
