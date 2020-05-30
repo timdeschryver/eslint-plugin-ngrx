91dc460c516acaa52ff5ff865c39481510d9885d
Julien Saguet <julien.saguet@outlook.fr>
2020-05-30 11:01:54 +0200

    feat: removes no-duplicate-action-in-reducer rule

--- src/rules/utils/helper-functions/index.ts
+++
@@ -1,5 +0,0 @@
-import { TSESTree } from '@typescript-eslint/experimental-utils'
-
-export function isIdentifier(node: TSESTree.Node): node is TSESTree.Identifier {
-  return node.type === 'Identifier'
-}
