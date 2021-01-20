d8d9bff8ac6215b80d54e2ccb5aca9340c90a392
Rafael Santana <rafaelsantana3095@gmail.com>
2021-01-20 09:22:18 -0300

    refactor: change action-hygiene pattern to ensure we report for correct function

--- src/utils/selectors/index.ts
+++ src/utils/selectors/index.ts
@@ -2,7 +2,7 @@ export const effectCreator = `ClassProperty[value.callee.name='createEffect']`
 
 export const effectDecorator = `ClassProperty > Decorator[expression.callee.name='Effect']`
 
-export const actionCreator = `CallExpression[callee.name=/createAction.*/]`
+export const actionCreator = `CallExpression[callee.name='createAction']`
 
 export const constructorExit = `MethodDefinition[kind='constructor']:exit`
 
