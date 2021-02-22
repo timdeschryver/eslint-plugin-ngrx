10352dcb50197476f8a2e1da871fc89f2373fb39
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2021-02-22 10:54:17 +0100

    Update src/utils/helper-functions/utils.ts
    Co-authored-by: Rafael Santana <rafaelsantana3095@gmail.com>

--- src/utils/helper-functions/utils.ts
+++ src/utils/helper-functions/utils.ts
@@ -89,45 +89,27 @@ export function getDecoratorArgument({ expression }: TSESTree.Decorator) {
 
 export function findNgRxStoreName(
   context: TSESLint.RuleContext<string, readonly unknown[]>,
-): string {
-  let storeName = ''
-  let i = 0
-  let node: TSESTree.Node
-  const ast = context.getSourceCode().ast
+): string | undefined {
+  const { ast } = context.getSourceCode()
+  const storeImportSpecifier = findImportDeclarationNode(ast, '@ngrx/store')
 
-  while (!storeName && (node = ast.body[i++])) {
-    if (!isImportDeclaration(node) || node.source.value !== '@ngrx/store')
-      continue
+  if (!storeImportSpecifier) return undefined
 
-    const storeImportSpecifier = node.specifiers
-      .map((specifierNode) =>
-        isImportSpecifier(specifierNode) &&
-        specifierNode.imported.name === 'Store'
-          ? specifierNode
-          : undefined,
-      )
-      .find(Boolean)
+  const [{ references }] = context.getDeclaredVariables(storeImportSpecifier)
 
-    if (!storeImportSpecifier) continue
-
-    const [storeImport] = context.getDeclaredVariables(storeImportSpecifier)
-    const storeImportName = storeImport.references
-      .map((ref) => {
-        if (
-          ref.identifier.parent &&
-          isTSTypeReference(ref.identifier.parent) &&
-          ref.identifier.parent.parent &&
-          isTSTypeAnnotation(ref.identifier.parent.parent) &&
-          ref.identifier.parent.parent.parent &&
-          isIdentifier(ref.identifier.parent.parent.parent)
-        ) {
-          return ref.identifier.parent.parent.parent.name
-        }
-        return ''
-      })
-      .find(Boolean)
-    storeName = storeImportName || ''
-  }
-
-  return storeName
+  return references
+    .map(({ identifier: { parent } }) => {
+      if (
+        parent &&
+        isTSTypeReference(parent) &&
+        parent.parent &&
+        isTSTypeAnnotation(parent.parent) &&
+        parent.parent.parent &&
+        isIdentifier(parent.parent.parent)
+      ) {
+        return parent.parent.parent.name
+      }
+      return undefined
+    })
+    .find(Boolean)
 }
