ae852ec41899ada42e9fca7efb3243088e603e20
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2021-05-15 17:03:01 +0200

    chore: switch to main branch (#143)

--- src/utils/helper-functions/docs.ts
+++ src/utils/helper-functions/docs.ts
@@ -1,2 +1,2 @@
 export const docsUrl = (ruleName: string) =>
-  `https://github.com/timdeschryver/eslint-plugin-ngrx/tree/master/docs/rules/${ruleName}.md`
+  `https://github.com/timdeschryver/eslint-plugin-ngrx/tree/main/docs/rules/${ruleName}.md`
