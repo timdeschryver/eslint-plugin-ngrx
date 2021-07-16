1ea360c3ad8a5bbc2edc5779b5170b7463a504c4
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2021-07-16 13:30:38 +0200

    feat(prefer-one-generic-in-create-for-feature-selector): add rule (#157)

---
+++ src/rules/store/prefer-one-generic-in-create-for-feature-selector.ts
@@ -0,0 +1,64 @@
+import path from 'path'
+import { ESLintUtils, TSESTree } from '@typescript-eslint/experimental-utils'
+
+import { docsUrl, isTSTypeReference, isIdentifier } from '../../utils'
+
+export const preferOneGenericInCreateForFeatureSelector =
+  'preferOneGenericInCreateForFeatureSelector'
+export const preferOneGenericInCreateForFeatureSelectorSuggest =
+  'preferOneGenericInCreateForFeatureSelectorSuggest'
+export type MessageIds =
+  | typeof preferOneGenericInCreateForFeatureSelector
+  | typeof preferOneGenericInCreateForFeatureSelectorSuggest
+
+type Options = []
+
+export default ESLintUtils.RuleCreator(docsUrl)<Options, MessageIds>({
+  name: path.parse(__filename).name,
+  meta: {
+    type: 'suggestion',
+    docs: {
+      category: 'Best Practices',
+      description: 'Prefer using a single generic to define the feature state.',
+      recommended: 'warn',
+    },
+    schema: [],
+    messages: {
+      [preferOneGenericInCreateForFeatureSelector]:
+        'Prefer using a single generic to define the feature state',
+      [preferOneGenericInCreateForFeatureSelectorSuggest]:
+        'Remove global state generic',
+    },
+  },
+  defaultOptions: [],
+  create: (context) => {
+    return {
+      [`CallExpression[callee.name="createFeatureSelector"] TSTypeParameterInstantiation[params.length>1]`](
+        node: TSESTree.TSTypeParameterInstantiation,
+      ) {
+        context.report({
+          node,
+          messageId: preferOneGenericInCreateForFeatureSelector,
+          suggest: [
+            {
+              fix: (fixer) => {
+                const [, featureState] = node.params
+                if (
+                  isTSTypeReference(featureState) &&
+                  isIdentifier(featureState.typeName)
+                ) {
+                  return fixer.replaceText(
+                    node,
+                    `<${featureState.typeName.name}>`,
+                  )
+                }
+                return []
+              },
+              messageId: preferOneGenericInCreateForFeatureSelectorSuggest,
+            },
+          ],
+        })
+      },
+    }
+  },
+})
