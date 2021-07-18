3d580266fee64b10a3190d4e529f9fc0e0633e12
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-07-18 10:12:17 +0200

    refactor: review changes

--- src/rules/store/avoid-combining-selectors.ts
+++ src/rules/store/avoid-combining-selectors.ts
@@ -3,10 +3,9 @@ import { ESLintUtils, TSESTree } from '@typescript-eslint/experimental-utils'
 
 import {
   docsUrl,
-  isCallExpression,
-  isMemberExpression,
-  isIdentifier,
   findNgRxStoreName,
+  storeSelect,
+  storeExpression,
 } from '../../utils'
 
 export const messageId = 'avoidCombiningSelectors'
@@ -35,51 +34,21 @@ export default ESLintUtils.RuleCreator(docsUrl)<Options, MessageIds>({
     const storeName = findNgRxStoreName(context)
     if (!storeName) return {}
 
+    const pipeableOrStoreSelect = `:matches(${storeExpression(
+      storeName,
+    )}[callee.property.name='pipe']:has(CallExpression[callee.name='select']), ${storeSelect(
+      storeName,
+    )})`
+
     return {
-      [`CallExpression[callee.name='combineLatest'][arguments.length>1]`](
+      [`CallExpression[callee.name='combineLatest'][arguments.length>1] ${pipeableOrStoreSelect} ~ ${pipeableOrStoreSelect}`](
         node: TSESTree.CallExpression,
       ) {
-        const [, ...violations] = node.arguments.filter(
-          (n) => selectMethod(n, storeName) || selectPipe(n, storeName),
-        )
-
-        for (const node of violations) {
-          context.report({
-            node,
-            messageId,
-          })
-        }
+        context.report({
+          node,
+          messageId,
+        })
       },
     }
   },
 })
-
-function selectMethod(node: TSESTree.Expression, storeName: string) {
-  return (
-    isCallExpression(node) &&
-    isMemberExpression(node.callee) &&
-    isMemberExpression(node.callee.object) &&
-    isIdentifier(node.callee.object.property) &&
-    node.callee.object.property.name === storeName &&
-    isIdentifier(node.callee.property) &&
-    node.callee.property.name === 'select'
-  )
-}
-
-function selectPipe(node: TSESTree.Expression, storeName: string) {
-  return (
-    isCallExpression(node) &&
-    isMemberExpression(node.callee) &&
-    isMemberExpression(node.callee.object) &&
-    isIdentifier(node.callee.object.property) &&
-    node.callee.object.property.name === storeName &&
-    isIdentifier(node.callee.property) &&
-    node.callee.property.name === 'pipe' &&
-    node.arguments.some(
-      (a) =>
-        isCallExpression(a) &&
-        isIdentifier(a.callee) &&
-        a.callee.name === 'select',
-    )
-  )
-}
