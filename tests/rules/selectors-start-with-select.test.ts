1caf591bdcbfb66f115f2d062640065caf8cd187
Chris McArthur <prince.chrismc@gmail.com>
2021-08-21 09:25:27 -0400

    tests: remove extra interface + more invalid cases

--- tests/rules/selectors-start-with-select.test.ts
+++ tests/rules/selectors-start-with-select.test.ts
@@ -9,48 +9,32 @@ import { ruleTester } from '../utils'
 ruleTester().run(path.parse(__filename).name, rule, {
   valid: [
     `
-    interface AppState {
-      feature: FeatureState;
-    }
-
     export const selectFeature:MemoizedSelector<any, any> = (state: AppState) => state.feature`,
     `
-    interface AppState {
-      feature: FeatureState;
-    }
-
     export const selectFeature = createSelector((state: AppState) => state.feature)`,
     `
-   export const featureKey = 'feature';
-
-    export interface FeatureState {
-      counter: number;
-    }
-
-    export interface AppState {
-      feature: FeatureState;
-    }
-
     export const selectFeature = createFeatureSelector<AppState, FeatureState>(featureKey);`,
   ],
   invalid: [
     fromFixture(
       stripIndent`
-        interface AppState {
-          feature: FeatureState;
-        }
-
         export const getFeature: MemoizedSelector<any, any> = (state: AppState) => state.feature
                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[${messageId}]`,
     ),
     fromFixture(
       stripIndent`
-        interface AppState {
-          feature: FeatureState;
-        }
-
         export const getFeature = createSelector((state: AppState) => state.feature)
                      ~~~~~~~~~~                        [${messageId}]`,
+    ),
+    fromFixture(
+      stripIndent`
+      export const feature = createFeatureSelector<AppState, FeatureState>(featureKey);
+                   ~~~~~~~                            [${messageId}]`,
+    ),
+    fromFixture(
+      stripIndent`
+        export const selectfeature = createSelector((state: AppState) => state.feature)
+                     ~~~~~~~~~~~~~                    [${messageId}]`,
     ),
   ],
 })
