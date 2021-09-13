baddd037adf18d304bf5dcf4384eefef379b80c1
Rafael Santana <rafaelsantana3095@gmail.com>
2021-09-13 07:01:01 -0300

    feat(use-consistent-global-store-name): add suggestion (#197)

--- src/rules/store/use-consistent-global-store-name.ts
+++ src/rules/store/use-consistent-global-store-name.ts
@@ -3,8 +3,12 @@ import { ESLintUtils } from '@typescript-eslint/experimental-utils'
 import path from 'path'
 import { docsUrl, injectedStore } from '../../utils'
 
-export const messageId = 'useConsistentGlobalStoreName'
-export type MessageIds = typeof messageId
+export const useConsistentGlobalStoreName = 'useConsistentGlobalStoreName'
+export const useConsistentGlobalStoreNameSuggest =
+  'useConsistentGlobalStoreNameSuggest'
+export type MessageIds =
+  | typeof useConsistentGlobalStoreName
+  | typeof useConsistentGlobalStoreNameSuggest
 
 type Options = [string]
 
@@ -24,28 +28,45 @@ export default ESLintUtils.RuleCreator(docsUrl)<Options, MessageIds>({
       },
     ],
     messages: {
-      [messageId]: 'Global store should have the name `{{ storeName }}`.',
+      [useConsistentGlobalStoreName]:
+        'Global store should have the name `{{ storeName }}`.',
+      [useConsistentGlobalStoreNameSuggest]: 'Use `{{ storeName }}`.',
     },
   },
   defaultOptions: ['store'],
   create: (context, [storeName]) => {
     return {
-      [injectedStore](node: TSESTree.Identifier) {
-        if (storeName && storeName !== node.name) {
-          context.report({
-            loc: {
-              ...node.loc,
-              end: {
-                ...node.loc.start,
-                column: node.loc.start.column + node.name.length,
-              },
+      [`${injectedStore}[name!=${storeName}]`]({
+        loc,
+        name,
+        range,
+        typeAnnotation,
+      }: TSESTree.Identifier & {
+        typeAnnotation: TSESTree.TSTypeAnnotation
+      }) {
+        const data = { storeName }
+        context.report({
+          loc: {
+            ...loc,
+            end: {
+              ...loc.start,
+              column: loc.start.column + name.length,
             },
-            messageId,
-            data: {
-              storeName,
+          },
+          messageId: useConsistentGlobalStoreName,
+          data,
+          suggest: [
+            {
+              messageId: useConsistentGlobalStoreNameSuggest,
+              fix: (fixer) =>
+                fixer.replaceTextRange(
+                  [range[0], typeAnnotation.range[0]],
+                  storeName,
+                ),
+              data,
             },
-          })
-        }
+          ],
+        })
       },
     }
   },
