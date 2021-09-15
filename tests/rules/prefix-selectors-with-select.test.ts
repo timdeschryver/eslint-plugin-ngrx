63a474e36625fd2b0590f9c79860ad2b493df7e7
Rafael Santana <rafaelsantana3095@gmail.com>
2021-09-14 23:57:54 -0300

    feat(prefix-selectors-with-select): add suggestion

--- tests/rules/prefix-selectors-with-select.test.ts
+++ tests/rules/prefix-selectors-with-select.test.ts
@@ -2,7 +2,8 @@ import { stripIndent } from 'common-tags'
 import { fromFixture } from 'eslint-etc'
 import path from 'path'
 import rule, {
-  messageId,
+  prefixSelectorsWithSelect,
+  prefixSelectorsWithSelectSuggest,
 } from '../../src/rules/store/prefix-selectors-with-select'
 import { ruleTester } from '../utils'
 
@@ -26,46 +27,140 @@ ruleTester().run(path.parse(__filename).name, rule, {
   invalid: [
     fromFixture(
       stripIndent`
-        export const getCount: MemoizedSelector<any, any> = (state: AppState) => state.feature
-                     ~~~~~~~~                        [${messageId}]`,
+      export const getCount: MemoizedSelector<any, any> = (state: AppState) => state.feature
+                   ~~~~~~~~ [${prefixSelectorsWithSelect}]
+      `,
+      {
+        suggestions: [
+          {
+            messageId: prefixSelectorsWithSelectSuggest,
+            data: {
+              name: 'selectCount',
+            },
+            output: stripIndent`
+            export const selectCount: MemoizedSelector<any, any> = (state: AppState) => state.feature`,
+          },
+        ],
+      },
     ),
     fromFixture(
       stripIndent`
-        export const getFeature = createSelector((state: AppState) => state.feature)
-                     ~~~~~~~~~~                        [${messageId}]`,
+      export const getF01 = createSelector((state: AppState) => state.feature)
+                   ~~~~~~ [${prefixSelectorsWithSelect}]
+      `,
+      {
+        suggestions: [
+          {
+            messageId: prefixSelectorsWithSelectSuggest,
+            output: stripIndent`
+            export const selectF01 = createSelector((state: AppState) => state.feature)`,
+          },
+        ],
+      },
     ),
     fromFixture(
       stripIndent`
-        export const select = (id: string) => createSelector(selectThings, things => things[id])
-                     ~~~~~~                            [${messageId}]`,
+      export const select = (id: string) => createSelector(selectThings, things => things[id])
+                   ~~~~~~ [${prefixSelectorsWithSelect}]
+      `,
+      {
+        suggestions: [
+          {
+            data: {
+              name: 'selectSelect',
+            },
+            messageId: prefixSelectorsWithSelectSuggest,
+            output: stripIndent`
+            export const selectSelect = (id: string) => createSelector(selectThings, things => things[id])`,
+          },
+        ],
+      },
     ),
     fromFixture(
       stripIndent`
-        export const testSelect = (id: string) => {
-                     ~~~~~~~~~~                        [${messageId}]
-          return createSelector(selectThings, things => things[id])
-        }`,
+      export const SELECT_TEST = (id: string) => {
+                   ~~~~~~~~~~~ [${prefixSelectorsWithSelect}]
+        return createSelector(selectThings, things => things[id])
+      }`,
+      {
+        suggestions: [
+          {
+            messageId: prefixSelectorsWithSelectSuggest,
+            data: {
+              name: 'selectSELECT_TEST',
+            },
+            output: stripIndent`
+            export const selectSELECT_TEST = (id: string) => {
+              return createSelector(selectThings, things => things[id])
+            }`,
+          },
+        ],
+      },
     ),
     fromFixture(
       stripIndent`
       export const feature = createFeatureSelector<AppState, FeatureState>(featureKey)
-                   ~~~~~~~                             [${messageId}]`,
+                   ~~~~~~~ [${prefixSelectorsWithSelect}]
+      `,
+      {
+        suggestions: [
+          {
+            messageId: prefixSelectorsWithSelectSuggest,
+            data: {
+              name: 'selectFeature',
+            },
+            output: stripIndent`
+            export const selectFeature = createFeatureSelector<AppState, FeatureState>(featureKey)`,
+          },
+        ],
+      },
     ),
     fromFixture(
       stripIndent`
-        export const selectfeature = createSelector((state: AppState) => state.feature)
-                     ~~~~~~~~~~~~~                     [${messageId}]`,
+      export const selectfeature = createSelector((state: AppState) => state.feature)
+                   ~~~~~~~~~~~~~ [${prefixSelectorsWithSelect}]
+      `,
+      {
+        suggestions: [
+          {
+            messageId: prefixSelectorsWithSelectSuggest,
+            data: {
+              name: 'selectFeature',
+            },
+            output: stripIndent`
+            export const selectFeature = createSelector((state: AppState) => state.feature)`,
+          },
+        ],
+      },
     ),
     fromFixture(
       stripIndent`
-        export const createSelect = createSelectorFactory((projectionFun) =>
-                     ~~~~~~~~~~~~                     [${messageId}]
-          defaultMemoize(
-            projectionFun,
-            orderDoesNotMatterComparer,
-            orderDoesNotMatterComparer,
-          ),
-        )`,
+      export const createSelect = createSelectorFactory((projectionFun) =>
+                   ~~~~~~~~~~~~ [${prefixSelectorsWithSelect}]
+        defaultMemoize(
+          projectionFun,
+          orderDoesNotMatterComparer,
+          orderDoesNotMatterComparer,
+        ),
+      )`,
+      {
+        suggestions: [
+          {
+            messageId: prefixSelectorsWithSelectSuggest,
+            data: {
+              name: 'selectSelect',
+            },
+            output: stripIndent`
+            export const selectSelect = createSelectorFactory((projectionFun) =>
+              defaultMemoize(
+                projectionFun,
+                orderDoesNotMatterComparer,
+                orderDoesNotMatterComparer,
+              ),
+            )`,
+          },
+        ],
+      },
     ),
   ],
 })
