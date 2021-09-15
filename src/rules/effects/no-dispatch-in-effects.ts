f40690e437c98f217a894c09f19b5e542527f11b
Rafael Santana <rafaelsantana3095@gmail.com>
2021-09-15 19:08:24 -0300

    feat(no-dispatch-in-effects): add suggestion

--- src/rules/effects/no-dispatch-in-effects.ts
+++ src/rules/effects/no-dispatch-in-effects.ts
@@ -1,12 +1,24 @@
-import type { TSESTree } from '@typescript-eslint/experimental-utils'
+import type { TSESLint, TSESTree } from '@typescript-eslint/experimental-utils'
 import { ESLintUtils } from '@typescript-eslint/experimental-utils'
 import path from 'path'
-import { dispatchInEffects, docsUrl, findNgRxStoreName } from '../../utils'
+import {
+  dispatchInEffects,
+  docsUrl,
+  findNgRxStoreName,
+  isArrowFunctionExpression,
+  isReturnStatement,
+} from '../../utils'
 
-export const messageId = 'NoDispatchInEffects'
-export type MessageIds = typeof messageId
+export const noDispatchInEffects = 'noDispatchInEffects'
+export const noDispatchInEffectsSuggest = 'noDispatchInEffectsSuggest'
+export type MessageIds =
+  | typeof noDispatchInEffects
+  | typeof noDispatchInEffectsSuggest
 
 type Options = []
+type DispatchInEffects = TSESTree.MemberExpression & {
+  parent: TSESTree.CallExpression
+}
 
 export default ESLintUtils.RuleCreator(docsUrl)<Options, MessageIds>({
   name: path.parse(__filename).name,
@@ -16,10 +28,13 @@ export default ESLintUtils.RuleCreator(docsUrl)<Options, MessageIds>({
       category: 'Possible Errors',
       description: 'An `Effect` should not call `store.dispatch`.',
       recommended: 'warn',
+      suggestion: true,
     },
     schema: [],
     messages: {
-      [messageId]: 'Calling `store.dispatch` in an `Effect` is forbidden.',
+      [noDispatchInEffects]:
+        'Calling `store.dispatch` in an `Effect` is forbidden.',
+      [noDispatchInEffectsSuggest]: 'Remove `store.dispatch`.',
     },
   },
   defaultOptions: [],
@@ -28,12 +43,29 @@ export default ESLintUtils.RuleCreator(docsUrl)<Options, MessageIds>({
     if (!storeName) return {}
 
     return {
-      [dispatchInEffects(storeName)](node: TSESTree.MemberExpression) {
+      [dispatchInEffects(storeName)](node: DispatchInEffects) {
         context.report({
           node,
-          messageId,
+          messageId: noDispatchInEffects,
+          suggest: [
+            {
+              messageId: noDispatchInEffectsSuggest,
+              fix: (fixer) => getFixes(fixer, node),
+            },
+          ],
         })
       },
     }
   },
 })
+
+function getFixes(fixer: TSESLint.RuleFixer, node: DispatchInEffects) {
+  const { parent } = node
+  const { parent: grandParent } = parent
+  const nodeToRemove =
+    grandParent &&
+    (isArrowFunctionExpression(grandParent) || isReturnStatement(grandParent))
+      ? node
+      : parent
+  return fixer.remove(nodeToRemove)
+}
--- tests/rules/no-dispatch-in-effects.test.ts
+++ tests/rules/no-dispatch-in-effects.test.ts
@@ -1,7 +1,9 @@
-import { stripIndent } from 'common-tags'
-import { fromFixture } from 'eslint-etc'
+import { stripIndent, stripIndents } from 'common-tags'
 import path from 'path'
-import rule, { messageId } from '../../src/rules/effects/no-dispatch-in-effects'
+import rule, {
+  noDispatchInEffects,
+  noDispatchInEffectsSuggest,
+} from '../../src/rules/effects/no-dispatch-in-effects'
 import { ruleTester } from '../utils'
 
 ruleTester().run(path.parse(__filename).name, rule, {
@@ -10,43 +12,151 @@ ruleTester().run(path.parse(__filename).name, rule, {
     import { Store } from '@ngrx/store'
     @Injectable()
     export class FixtureEffects {
+      effectOK = createEffect(() => this.actions.pipe(
+        ofType('PING'),
+        map(() => ({ type: 'PONG' }))
+      ))
 
-    effectOK = createEffect(() => this.actions.pipe(
-      ofType('PING'),
-      map(() => ({ type: 'PONG' }))
-    ))
-
-    constructor(private actions: Actions, private store: Store){}
+      constructor(private actions: Actions, private store: Store) {}
     }`,
   ],
   invalid: [
-    fromFixture(
-      stripIndent`
+    {
+      code: stripIndent`
       import { Store } from '@ngrx/store'
       @Injectable()
       export class FixtureEffects {
         effectNOK = createEffect(() => this.actions.pipe(
           ofType('PING'),
           map(() => this.store.dispatch({ type: 'PONG' }))
-                    ~~~~~~~~~~~~~~~~~~~ [${messageId}]
-          ), { dispatch: false })
+        ), { dispatch: false })
 
-        constructor(private actions: Actions, private store: Store){}
+        constructor(private actions: Actions, private store: Store) {}
       }`,
-    ),
-    fromFixture(
-      stripIndent`
+      errors: [
+        {
+          column: 15,
+          endColumn: 34,
+          line: 6,
+          messageId: noDispatchInEffects,
+          suggestions: [
+            {
+              messageId: noDispatchInEffectsSuggest,
+              output: stripIndent`
+            import { Store } from '@ngrx/store'
+            @Injectable()
+            export class FixtureEffects {
+              effectNOK = createEffect(() => this.actions.pipe(
+                ofType('PING'),
+                map(() => ({ type: 'PONG' }))
+              ), { dispatch: false })
+
+              constructor(private actions: Actions, private store: Store) {}
+            }`,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: stripIndent`
       import { Store } from '@ngrx/store'
       @Injectable()
       export class FixtureEffects {
-        effectNOK = createEffect(() => this.actions.pipe(
-          ofType('PING'),
-          map(() => this.customName.dispatch({ type: 'PONG' }))
-                    ~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
-          ), { dispatch: false })
+        effectNOK = createEffect(
+          () =>
+            this.actions.pipe(
+              ofType('PING'),
+              map(() => {
+                return this.customName.dispatch({ /* you shouldn't do this */ type: 'PONG' })
+              }),
+            ),
+          { dispatch: false },
+        )
 
-        constructor(private actions: Actions, private customName: Store){}
+        constructor(private actions: Actions, private customName: Store) {}
       }`,
-    ),
+      errors: [
+        {
+          column: 18,
+          endColumn: 42,
+          line: 9,
+          messageId: noDispatchInEffects,
+          suggestions: [
+            {
+              messageId: noDispatchInEffectsSuggest,
+              output: stripIndent`
+              import { Store } from '@ngrx/store'
+              @Injectable()
+              export class FixtureEffects {
+                effectNOK = createEffect(
+                  () =>
+                    this.actions.pipe(
+                      ofType('PING'),
+                      map(() => {
+                        return ({ /* you shouldn't do this */ type: 'PONG' })
+                      }),
+                    ),
+                  { dispatch: false },
+                )
+
+                constructor(private actions: Actions, private customName: Store) {}
+              }`,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: stripIndents`
+      import { Store } from '@ngrx/store'
+      @Injectable()
+      export class FixtureEffects {
+        effectNOK = createEffect(
+          () =>
+            this.actions.pipe(
+              ofType('PING'),
+              map(() => {
+                this.store$.dispatch(someAction())// you shouldn't do this
+                return anotherAction()
+              }),
+            ),
+          { dispatch: false },
+        )
+
+        constructor(private actions: Actions, private readonly store$: Store) {}
+      }`,
+      errors: [
+        {
+          column: 1,
+          endColumn: 21,
+          line: 9,
+          messageId: noDispatchInEffects,
+          suggestions: [
+            {
+              messageId: noDispatchInEffectsSuggest,
+              output: stripIndents`
+              import { Store } from '@ngrx/store'
+              @Injectable()
+              export class FixtureEffects {
+                effectNOK = createEffect(
+                  () =>
+                    this.actions.pipe(
+                      ofType('PING'),
+                      map(() => {
+                        // you shouldn't do this
+                        return anotherAction()
+                      }),
+                    ),
+                  { dispatch: false },
+                )
+
+                constructor(private actions: Actions, private readonly store$: Store) {}
+              }`,
+            },
+          ],
+        },
+      ],
+    },
   ],
 })
