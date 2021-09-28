b7c395a45557da70acd4f808e44eef9eb80bb7ea
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-09-28 17:22:01 +0200

    chore: remove internal rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,20 +6,18 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  plugins: ['internal-rules'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
     project: './tsconfig.eslint.json',
     extraFileExtensions: ['.json'],
   },
-  ignorePatterns: ['internal-rules/out/*.js', '.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unnecessary-type-assertion': 'error',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/prefer-reduce-type-parameter': 'error',
-    'internal-rules/first-rule': 'error',
   },
 }
