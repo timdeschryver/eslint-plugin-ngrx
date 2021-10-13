851b266125eda058e937d28f8fac85826338cc12
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-10-13 20:49:08 +0200

    fix indention

--- tests/rules/avoid-combining-selectors.test.ts
+++ tests/rules/avoid-combining-selectors.test.ts
@@ -11,42 +11,42 @@ const valid = [
 import { Store } from '@ngrx/store'
 
 class Ok {
-readonly test$ = somethingOutside();
+  readonly test$ = somethingOutside();
 }`,
   `
 import { Store } from '@ngrx/store'
 
 class Ok1 {
-readonly vm$: Observable<unknown>
+  readonly vm$: Observable<unknown>
 
-constructor(store: Store) {
-  this.vm$ = store.select(selectItems)
-}
+  constructor(store: Store) {
+    this.vm$ = store.select(selectItems)
+  }
 }`,
   `
 import { Store, select } from '@ngrx/store'
 
 class Ok2 {
-vm$ = this.store.pipe(select(selectItems))
+  vm$ = this.store.pipe(select(selectItems))
 
-constructor(private store: Store) {}
+  constructor(private store: Store) {}
 }`,
   `
 import { Store } from '@ngrx/store'
 
 class Ok3 {
-vm$ = combineLatest(this.store$.select(selectItems), this.somethingElse())
+  vm$ = combineLatest(this.store$.select(selectItems), this.somethingElse())
 
-constructor(private store$: Store) {}
+  constructor(private store$: Store) {}
 }`,
   `
 import { Store } from '@ngrx/store'
 
 @Pipe()
 class Ok4 {
-vm$ = combineLatest(this.somethingElse(), this.store.select(selectItems))
+  vm$ = combineLatest(this.somethingElse(), this.store.select(selectItems))
 
-constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store) {}
 }`,
 ]
 
@@ -56,17 +56,17 @@ const invalid = [
 import { Store } from '@ngrx/store'
 
 class NotOk {
-readonly vm$: Observable<unknown>
+  readonly vm$: Observable<unknown>
 
-constructor(store: Store, private store2: Store) {
-  this.vm$ = combineLatest(
-    store.select(selectItems),
-    store.select(selectOtherItems),
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
-    this.store2.select(selectOtherItems),
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
-  )
-}
+  constructor(store: Store, private store2: Store) {
+    this.vm$ = combineLatest(
+      store.select(selectItems),
+      store.select(selectOtherItems),
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
+      this.store2.select(selectOtherItems),
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
+    )
+  }
 }`,
   ),
   fromFixture(
@@ -74,13 +74,13 @@ constructor(store: Store, private store2: Store) {
 import { Store } from '@ngrx/store'
 
 class NotOk1 {
-vm$ = combineLatest(
-  this.store.pipe(select(selectItems)),
-  this.store.pipe(select(selectOtherItems)),
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
-)
+  vm$ = combineLatest(
+    this.store.pipe(select(selectItems)),
+    this.store.pipe(select(selectOtherItems)),
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
+  )
 
-constructor(private store: Store) {}
+  constructor(private store: Store) {}
 }`,
   ),
   fromFixture(
@@ -88,14 +88,14 @@ constructor(private store: Store) {}
 import { Store } from '@ngrx/store'
 
 class NotOk2 {
-vm$ = combineLatest(
-  this.customName.select(selectItems),
-  this.customName.select(selectOtherItems),
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
-  this.somethingElse(),
-)
+  vm$ = combineLatest(
+    this.customName.select(selectItems),
+    this.customName.select(selectOtherItems),
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
+    this.somethingElse(),
+  )
 
-constructor(private customName: Store) {}
+  constructor(private customName: Store) {}
 }`,
   ),
   fromFixture(
@@ -104,14 +104,14 @@ import { Store } from '@ngrx/store'
 
 @Pipe()
 class NotOk3 {
-vm$ = combineLatest(
-  this.customName.select(selectItems),
-  this.customName.pipe(select(selectOtherItems)),
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
-  this.somethingElse(),
-)
+  vm$ = combineLatest(
+    this.customName.select(selectItems),
+    this.customName.pipe(select(selectOtherItems)),
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
+    this.somethingElse(),
+  )
 
-constructor(private readonly customName: Store) {}
+  constructor(private readonly customName: Store) {}
 }`,
   ),
   fromFixture(
@@ -119,13 +119,13 @@ constructor(private readonly customName: Store) {}
 import { Store } from '@ngrx/store'
 
 class NotOk4 {
-vm$ = combineLatest(
-  this.store.pipe(select(selectItems)),
-  this.store.select(selectOtherItems),
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
-)
+  vm$ = combineLatest(
+    this.store.pipe(select(selectItems)),
+    this.store.select(selectOtherItems),
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
+  )
 
-constructor(private store: Store) {}
+  constructor(private store: Store) {}
 }`,
   ),
 
@@ -134,15 +134,15 @@ constructor(private store: Store) {}
 import { Store } from '@ngrx/store'
 
 class NotOk5 {
-vm$ = combineLatest(
-  this.store.select(selectItems),
-  this.store.pipe(select(selectOtherItems)),
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
-  this.store2.pipe(select(selectOtherItems)),
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
-)
+  vm$ = combineLatest(
+    this.store.select(selectItems),
+    this.store.pipe(select(selectOtherItems)),
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
+    this.store2.pipe(select(selectOtherItems)),
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ [${messageId}]
+  )
 
-constructor(private store: Store, private store2: Store) {}
+  constructor(private store: Store, private store2: Store) {}
 }`,
   ),
 ]
