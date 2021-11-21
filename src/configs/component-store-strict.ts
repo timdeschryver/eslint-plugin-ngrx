546837b4e50acbd027e47686974c8af06362db17
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-11-21 19:33:22 +0100

    fix: only require parseroptions when neede

--- src/configs/component-store-strict.ts
+++ src/configs/component-store-strict.ts
@@ -6,11 +6,7 @@
 
 export = {
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
+
   plugins: ['ngrx'],
   rules: { 'ngrx/updater-explicit-return-type': 'error' },
 }
